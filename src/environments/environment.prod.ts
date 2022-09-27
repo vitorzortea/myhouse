13c6437dac4f47d43d3988abadcd117222218f4f
Vitor Zortea <vitor.zortea@wkconsultoria.com.br>
2022-09-27 10:04:02 -0300

    change enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDBLQNRcQIPt0tb_EhlgxGcKZqhWwgKeKQ",
+    authDomain: "myhome-e2107.firebaseapp.com",
+    projectId: "myhome-e2107",
+    storageBucket: "myhome-e2107.appspot.com",
+    messagingSenderId: "418425448140",
+    appId: "1:418425448140:web:1d0ae5ff01093996770469"
+  }
 };
